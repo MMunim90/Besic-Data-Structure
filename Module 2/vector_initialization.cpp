f4d593f9ad9ec57f75665947bf0a4fde03098e7e
MMunim <mmunim9.9.01@gmail.com>
2025-12-17 19:29:41 +0600

    explore vector initialization

---
+++ Module 2/vector_initialization.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // type 1
+    // vector<int>n;
+
+    // type 2
+    // vector<int>n(9);
+
+    // type 3
+    // vector<int>n(9, 1);
+
+    // for(int i=0; i<n.size(); i++)
+    // {
+    //     cout << n[i] << " ";
+    // }
+
+
+    // type 4
+    // vector<int>n(9, 9);
+    // vector<int>v(n);
+
+    // for(int i=0; i<v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+    // type 5
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // vector<int>v(arr, arr+5);
+
+    // for(int i=0; i<v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+
+    // type 6
+    vector<int>v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    // cout << n.size() << endl;
+    return 0;
+}
