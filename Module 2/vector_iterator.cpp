d7ff1f83eb6dbad560602c5363867382bbd176fe
MMunim <mmunim9.9.01@gmail.com>
2025-12-18 20:43:00 +0600

    explore vector iterator

---
+++ Module 2/vector_iterator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+
+    for(auto itr=v.begin(); itr<v.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
+    return 0;
+}
