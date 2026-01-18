69ac7a5116479e2a07d9f280405a727ee79ed311
MMunim <mmunim9.9.01@gmail.com>
2026-01-18 21:48:51 +0600

    contest perfect root and prefix max problem solved problem solved

--- road to xpsc/A_Phone_Desktop.cpp
+++ road to xpsc/A_Phone_Desktop.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 int main()
 {
+    int t; cin >> t;
+    while (t--)
+    {
+        int x, y; cin >> x >> y;
+
+        int total_cell = x+(y*4);
+        int screen = y/2;
+        int remaining_cell = screen*7;
+
+        if(y%2 != 0)
+        {
+            screen++;
+            remaining_cell += 11;
+        }
+
+        if(x > remaining_cell)
+        {
+            x -= remaining_cell;
+            screen += x/15;
+            if(x%15 != 0)
+            {
+                screen++;
+            }
+        }
+
+         cout << screen << endl;
+    }
     
     return 0;
 }
