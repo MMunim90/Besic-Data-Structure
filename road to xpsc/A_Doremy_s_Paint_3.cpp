b0f4025f4afb0f426342fcfe2840e356eb519835
MMunim <mmunim9.9.01@gmail.com>
2026-01-22 00:20:41 +0600

    doremy paint problem solved

--- road to xpsc/A_Doremy_s_Paint_3.cpp
+++ road to xpsc/A_Doremy_s_Paint_3.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 int main()
 {
+    int t; cin >> t;
+    while (t--)
+    {
+        map<int, int> mp;
+        int n; cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            mp[arr[i]]++;
+        }
+
+        if(mp.size() <= 2)
+        {
+            if(mp.size() == 2)
+            {
+                vector<int> v;
+                for(auto p : mp)
+                {
+                    v.push_back(p.second);
+                }
+                if(abs(v[0] - v[1]) > 1) cout << "No\n";
+                else cout << "Yes\n";
+            }
+            else
+                cout << "Yes" << endl;
+        }
+        else
+            cout << "No" << endl;
+    }
     
     return 0;
 }
