885e05b7aac65cc3037d248029d38bd3db672f13
MMunim <mmunim9.9.01@gmail.com>
2026-01-23 22:45:41 +0600

    Segregate Even and Odd Nodes problem solved

--- ds grand contest/segregate_even_odd_nodes.cpp
+++ ds grand contest/segregate_even_odd_nodes.cpp
@@ -1,7 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    vector<int> even;
+    vector<int> odd;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] % 2 == 0)
+            even.push_back(v[i]);
+        else
+            odd.push_back(v[i]);
+    }
+
+    for (int i = 0; i < even.size(); i++)
+    {
+        cout << even[i] << " ";
+    }
+    for (int i = 0; i < odd.size(); i++)
+    {
+        cout << odd[i] << " ";
+    }
+
+    cout << endl;
     return 0;
 }
