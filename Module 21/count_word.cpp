7cc3dceb51905a373c304c8dd6d0600d48a20ec1
MMunim <mmunim9.9.01@gmail.com>
2026-02-08 00:35:58 +0600

    count word problem solve using map

---
+++ Module 21/count_word.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+    string word;
+
+    map<string, int> mp;
+    while (ss >> word) // NlogN
+    {
+        // cout << word << endl;
+        mp[word]++; // logN
+    }
+    
+    for(auto it=mp.begin(); it!=mp.end(); it++) //NlogN
+    {
+        cout << it->first << " = " << it->second << endl; //logN
+    }
+    return 0;
+}
