85f2ec2ed24eef48e3b931691fce3f7a3e6ac4fb
MMunim <mmunim9.9.01@gmail.com>
2026-02-07 12:58:41 +0600

    singly2 problem solved

---
+++ all practice problem/singly2.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    Node* temp = head;
+
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    tail = newNode;
+}
+
+void reverse_list(Node* temp)
+{
+    if(temp == NULL)
+        return;
+    reverse_list(temp->next);
+    cout << temp->val << " ";
+}
+
+void print_list(Node* temp)
+{
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true)
+    {
+        int val; cin >> val;
+
+        if(val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    // print_list(head);
+    // cout << endl;
+    reverse_list(head);
+
+    return 0;
+}
